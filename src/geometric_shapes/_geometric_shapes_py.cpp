aa103605e8553f52f4411df4e1aa8084fcc66343
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2024-01-02 12:36:00 +0100

    Add copyright headers

--- src/geometric_shapes/_geometric_shapes_py.cpp
+++ src/geometric_shapes/_geometric_shapes_py.cpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+ * Software License Agreement (BSD 3-Clause License)
+ *
+ *  Copyright (c) 2024, VBTI
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Matthijs van der Burgh */
+
 #include <pybind11/pybind11.h>
 
 //#include "aabb.hpp"
@@ -5,46 +39,6 @@
 #include "shape_operations.hpp"
 #include "shape_to_marker.hpp"
 #include "shapes.hpp"
-
-//#include <rcl/domain_id.h>
-//#include <rcl/service_introspection.h>
-//#include <rcl/time.h>
-//#include <rcl_action/types.h>
-//
-//#include <rmw/qos_profiles.h>
-//#include <rmw/time.h>
-
-//#include "action_client.hpp"
-//#include "action_goal_handle.hpp"
-//#include "action_server.hpp"
-//#include "client.hpp"
-//#include "clock.hpp"
-//#include "context.hpp"
-//#include "destroyable.hpp"
-//#include "duration.hpp"
-//#include "clock_event.hpp"
-//#include "event_handle.hpp"
-//#include "exceptions.hpp"
-//#include "graph.hpp"
-//#include "guard_condition.hpp"
-//#include "lifecycle.hpp"
-//#include "logging.hpp"
-//#include "logging_api.hpp"
-//#include "names.hpp"
-//#include "node.hpp"
-//#include "publisher.hpp"
-//#include "qos.hpp"
-//#include "serialization.hpp"
-//#include "service.hpp"
-//#include "service_info.hpp"
-//#include "service_introspection.hpp"
-//#include "signal_handler.hpp"
-//#include "subscription.hpp"
-//#include "time_point.hpp"
-//#include "timer.hpp"
-//#include "type_description_service.hpp"
-//#include "utils.hpp"
-//#include "wait_set.hpp"
 
 namespace py = pybind11;
 
